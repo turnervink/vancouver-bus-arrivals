5306b1507a7b3c545b5848baec993683cccafd85
Turner Vink <turner@turnervink.com>
2019-02-08 15:11:19 -0800

    Get next arrivals for the closest stop

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@
 
 const functions = require('firebase-functions');
 const rp = require('request-promise');
-const {dialogflow, Suggestions, Permission} = require('actions-on-google');
+const {dialogflow, Suggestions, Permission, SimpleResponse} = require('actions-on-google');
 
 const tlBaseUrl = 'http://api.translink.ca/rttiapi/v1';
 const tlApiKey = 'hCnIQTl1g1LNlWOZhEfa';
@@ -16,7 +16,8 @@ function getStopsAtCoordinates(lat, long) {
         url: `${tlBaseUrl}/stops?apikey=${tlApiKey}&lat=${lat}&long=${long}`,
         headers: {
             'Content-Type': 'application/json'
-        }
+        },
+        simple: false
     };
 
     return rp(options);
@@ -27,23 +28,27 @@ function getArrivalsAtStop(stopNo, count) {
         url: `${tlBaseUrl}/stops/${stopNo}/estimates?apikey=${tlApiKey}&count=${count}`,
         headers: {
             'Content-Type': 'application/json'
-        }
+        },
+        simple: false
     };
 
     return rp(options);
 }
 
-function getNearestArrivals(lat, long) {
+function getNearestArrivals(lat, long, count, routeNo, terminus) {
     return new Promise((resolve, reject) => {
         getStopsAtCoordinates(lat, long).then((stops) => {
-            console.log('STOPS', stops);
+            // TODO If routeNo or terminus is given filter the returned stops
+
             stops = JSON.parse(stops);
             let nearestStop = stops[0].StopNo;
 
-            getArrivalsAtStop(nearestStop, 3).then((arrivals) => {
-                console.log('ARRIVALS', arrivals);
+            getArrivalsAtStop(nearestStop, count).then((arrivals) => {
                 arrivals = JSON.parse(arrivals);
-                resolve(arrivals);
+                resolve({
+                    nearestStop: stops[0],
+                    arrivals: arrivals
+                });
             }).catch((err) => reject(err));
         }).catch((err) => reject(err));
     });
@@ -76,19 +81,39 @@ app.intent('Get arrivals near user', (conv, params, permissionGranted) => {
     if (requestedPermission === 'DEVICE_PRECISE_LOCATION') {
         const {coordinates} = conv.device.location;
 
-        return getNearestArrivals((coordinates.latitude).toFixed(6), (coordinates.longitude).toFixed(6)).then((arrivals) => {
-            console.log(arrivals);
+        // TODO Look at parameters to determine what to pass to getNearestArrivals, or call getArrivalsNearLocation
 
-            let speech = ``;
-            let text = ``;
+        // No information on route/terminus was given; find the next arrival of each route at the stop closest to the user
+        // TODO Will unspecified parameters be null so we can just pass them to getNearestArrivals and deal with them there?
+        return getNearestArrivals((coordinates.latitude).toFixed(6), (coordinates.longitude).toFixed(6), 1, null, null).then((results) => {
+            console.log(results);
 
-            arrivals.forEach((arrival, i) => {
-                arrival.Schedules.forEach((schedule, j) => {
+            if (!results.arrivals.Code) {
+                conv.ask(new SimpleResponse({
+                    speech: `Here are the next arrivals at the nearest stop, ${results.nearestStop.Name}`,
+                    text: `Here are the next arrivals at ${results.nearestStop.Name}`
+                }));
 
+                let speech = ``;
+                let text = ``;
+
+                results.arrivals.forEach((arrival, i) => {
+                    arrival.Schedules.forEach((schedule, j) => {
+                        speech += `<speak>The ${arrival.RouteNo} to ${schedule.Destination} in ${schedule.ExpectedCountdown} minutes. <break time="500ms" /> </speak>`;
+                        text += `${arrival.RouteNo} ${schedule.Destination} - ${schedule.ExpectedCountdown} minutes`;
+                        if (j !== arrival.Schedules.length - 1) text += '\n';
+                    });
                 });
-            });
 
-            conv.close('Check the logs');
+                conv.close(new SimpleResponse({
+                    speech: speech,
+                    text: text
+                }));
+            } else {
+                // TODO Write error code handing function
+                conv.close(`Sorry, no stop estimates could be found for ${results.nearestStop.Name}`);
+            }
+
         }).catch((err) => {
             console.error(err);
             conv.close('Sorry, something went wrong when trying to get the next arrivals.');
